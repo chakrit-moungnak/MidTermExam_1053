43109f99534b6d4e60d171864a0b30868059e9c7
chakrit <chakrit.moun@kmutt.ac.th>
2022-10-13 18:57:54 +0700

    Added Program.cs

--- Model/Currentstudent.cs
+++ Model/Currentstudent.cs
@@ -1,11 +1,14 @@
 public class Currentstudent : Attendance{
     private string studentID;
+
+    private string admin_position;
     private string currentstudentemail;
     private string currentstudentpassword;
 
     public Currentstudent(string name_prefix, string name, string surname, string studentID, string age, string allergy, string religion
-    , string currentstudentemail, string currentstudentpassword) : base(name_prefix, name, surname, age, allergy, religion) {
+    , string admin_position, string currentstudentemail, string currentstudentpassword) : base(name_prefix, name, surname, age, allergy, religion) {
         this.studentID = studentID;
+        this.admin_position = admin_position;
         this.currentstudentemail = currentstudentemail;
         this.currentstudentpassword = currentstudentpassword;
     }
--- Program.cs
+++ Program.cs
@@ -6,10 +6,25 @@ enum RegistrationType{
     CurrentStudent = 1, Student = 2, Teacher = 3
 }
 
+enum NamePrefix{
+    Mr = 1, Mrs = 2, Miss = 3
+}
+
+enum Religion{
+    Buddhism = 1, Christianity = 2, Islam = 3, Other = 4
+}
+
+enum Admin{
+    Yes = 1, No = 2
+}
+
 class Program {
 
-    static void Main(string[] args) {
+    static AttendanceList attendancelist;
 
+    static void Main(string[] args) {
+        AddAttendanceListWhenProgramIsLoad();
+        PrintMenuScreen();
     }
 
     static void PrintMenuScreen() {
@@ -119,7 +134,7 @@ class Program {
     }
 
     static void PrintHeaderCurrentStudentRegistration(){
-        Console.WriteLine(" Register new CurrentStudent ");
+        Console.WriteLine(" Register : CurrentStudent ");
         Console.WriteLine("---------------------------------------------------");
     }
 
@@ -127,7 +142,121 @@ class Program {
         Console.Clear();
         PrintHeaderCurrentStudentRegistration();
 
-        
+        Currentstudent currentstudent = new Currentstudent(InputNamePrefix(), InputName(), InputSurname(), 
+        InputStudentID(), InputAge(), InputAllergy(), InputReligion(), InputAdmin(), InputEmail(), InputPassword());
+    }
+
+    static void AddAttendanceListWhenProgramIsLoad(){
+        Program.attendancelist = new AttendanceList();
+    }
+
+    static void InputNamePrefix(){
+        Console.WriteLine(" Please select name prefix (1.Mr, 2.Mrs, 3.Miss): ");
+        NamePrefix prefix = (NamePrefix)(int.Parse(Console.ReadLine()));
+
+        SelectNamePrefix(prefix);
+    }
+
+    static void SelectNamePrefix(NamePrefix prefix){
+        switch(prefix) {
+            case NamePrefix.Mr : Console.Write("Mr. ");
+                break;
+            case NamePrefix.Mrs : Console.Write("Mrs. ");
+                break;
+            case NamePrefix.Miss : Console.Write("Miss. ");
+                break;
+            default :
+                break; 
+        }
+    }
+
+
+    static string InputName(){
+        Console.Write(" ");
+
+        return Console.ReadLine();
+    }
+
+    static string InputSurname(){
+        Console.Write(" ");
+
+        return Console.ReadLine();
+    }
+
+    static string InputStudentID(){
+        Console.WriteLine("StudentID : ");
+
+        return Console.ReadLine();
+    }
+
+    static string InputAge(){
+        Console.WriteLine("Age : ");
+
+        return Console.ReadLine();
+    }
+
+    static string InputAllergy(){
+        Console.WriteLine("Are you allergic to something? (Input allergic or none): ");
+
+        return Console.ReadLine();
+    }
+
+    static void InputReligion(){
+        Console.WriteLine(" Please select Religion (1.Buddhism, 2.christianity, 3.islam, 4.Other) : ");
+        Religion religion = (Religion)(int.Parse(Console.ReadLine()));
+
+        SelectReligion(religion);
+    }
+
+    static void SelectReligion(Religion religion){
+        switch(religion) {
+            case Religion.Buddhism : Console.Write("Buddhism ");
+                break;
+            case Religion.Christianity : Console.Write("Christianity ");
+                break;
+            case Religion.Islam : Console.Write("Islam ");
+                break;
+            case Religion.Other : InputReligionInfo();
+                break;
+            default :
+                break; 
+        }
+    }
+
+    static string InputReligionInfo(){
+        Console.WriteLine("Please input your religion : ");
+
+        return Console.ReadLine();
+    }
+
+    static void InputAdmin(){
+        Console.WriteLine("Are you an administrator? (1.Yes, 2.No): ");
+        Admin admin = (Admin)(int.Parse(Console.ReadLine()));
+
+        AdministratorConfirm(admin);
+    }
+
+    static void AdministratorConfirm(Admin admin){
+        switch(admin) {
+            case Admin.Yes : Console.Write("Yes I am ");
+                break;
+            case Admin.No : Console.Write("No ");
+                break;
+            default :
+                break; 
+        }
+    }
+
+    static string InputEmail(){
+        Console.WriteLine("Please input your Email (If you an administrator): ");
+
+        return Console.ReadLine();
+    }
+
+    static string InputPassword(){
+        Console.WriteLine("Please input your password (If you an administrator): ");
+
+        return Console.ReadLine();
     }
 }
 
